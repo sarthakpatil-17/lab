37aac3f99a0d0113d6ae453ebb5ab9001424930a
Sarthak Patil <sarthakrajeshpatil40@gmail.com>
2026-05-13 00:22:44 +0530

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,26 @@
+# Binary Search in Python
+
+arr = [10, 20, 30, 40, 50, 60, 70]
+target = 50
+
+low = 0
+high = len(arr) - 1
+
+found = False
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == target:
+        print("Element found at index", mid)
+        found = True
+        break
+
+    elif arr[mid] < target:
+        low = mid + 1
+
+    else:
+        high = mid - 1
+
+if not found:
+    print("Element not found")
